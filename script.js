2f2dd50fc0e003c3db5c1cd1e517fa530b156440
Sebastian Doe <sebastiandoe5@gmail.com>
2020-07-26 13:30:59 +0100

    How did I mess this up tho

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-$(document).ready(function(){
-    $(".celebrityOption").click(function(){
-        $('.resultText').text(celebrities[Math.floor(Math.random() * celebrities.length)]);
-    });
-});
-
 var celebrities = ["David Attenborough", "Kim Kardashian", "Donald Trump", "Oprah Winfrey", "Dwayne Johnson", "Leonardo DiCaprio", "Nicki Minaj", "Justin Bieber", "Taylor Swift", "Eminem", "Madonna", "Ed Sheeran", "Lady Gaga", "Elton John", "Kanye West", "Katy Perry", "Ariana Grande"];
