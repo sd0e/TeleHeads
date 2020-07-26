404b0731091d2a73e2cda09fb4ea314eef10d0ae
Sebastian Doe <sebastiandoe5@gmail.com>
2020-07-26 13:05:32 +0100

    Add basic functionality

---
+++ script.js
@@ -0,0 +1,1 @@
+var celebrities = ["David Attenborough", "Kim Kardashian", "Donald Trump", "Oprah Winfrey", "Dwayne Johnson", "Leonardo DiCaprio", "Nicki Minaj", "Justin Bieber", "Taylor Swift", "Eminem", "Madonna", "Ed Sheeran", "Lady Gaga", "Elton John", "Kanye West", "Katy Perry", "Ariana Grande"];
