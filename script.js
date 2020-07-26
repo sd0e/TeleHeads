da6fd95b88e2628225cc66113a1a11f6d36bc0d6
Sebastian Doe <sebastiandoe5@gmail.com>
2020-07-26 13:25:32 +0100

    Fix TV compatibility again

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+$(document).ready(function(){
+    $(".celebrityOption").click(function(){
+        $('.resultText').text(celebrities[Math.floor(Math.random() * celebrities.length)]);
+    });
+});
+
 var celebrities = ["David Attenborough", "Kim Kardashian", "Donald Trump", "Oprah Winfrey", "Dwayne Johnson", "Leonardo DiCaprio", "Nicki Minaj", "Justin Bieber", "Taylor Swift", "Eminem", "Madonna", "Ed Sheeran", "Lady Gaga", "Elton John", "Kanye West", "Katy Perry", "Ariana Grande"];
